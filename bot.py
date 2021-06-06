9c5a5e91b0553bae94ca6ff2e3e0f9489967c2a3
Jose Ernesto <josernesto1989@gmail.com>
2021-06-06 09:24:16 -0400

    cambiando dola a 70

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ PORT = int(os.environ.get('PORT', 5000))
 TOKEN = '1572067631:AAEsliD_kaAcdZ5JZx7OhnPmXAeOqUN_1Pc'
 
 
-dolarValue = 65.0
+dolarValue = 70.0
 
 creditsValues = {
     "magma": 1.02,
