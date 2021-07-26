d78aa79657cded368d4c8b03d503db34fd8d1998
Jose Ernesto <josernesto1989@gmail.com>
2021-07-26 12:33:15 -0400

    help

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def start(update: Update, context: CallbackContext):
   #                           reply_markup=main_menu_keyboard())
 
 def help(update: Update, context: CallbackContext):
-  text = "Los comandos disponibles son:\n"
+  text = "Version: 2.0\nLos comandos disponibles son:\n"
   text = text + "\n/help para este menú"
   text = text + "\n/cred <servicio> <cantidad de creditos>"
   text = text + "\n/dolar para saber el precio del dolar"
